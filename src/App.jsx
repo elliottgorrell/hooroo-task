53ecdece03604b2494305a1bb6f9b95e04909a7e
Elliott Gorrell <elliottgorrell1@gmail.com>
2018-10-05 16:16:07 +1000

    added header with dropdown for sorting

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <header className="AppHeader">
+          <img src={logo} className="AppLogo" alt="logo" />
+          <p style={{ gridRow: '2' }}><b>5</b> Hotels in <b>Sydney</b></p>
+          <div style={{ gridRow: '2'}}>
+            <select style={{ float: 'right', width: '100px' }}>
+              <option value="low">low-high</option>
+              <option value="high">high-low</option>
+            </select>
+          </div>
         </header>
         <AccomList />
       </div>
