a7a3a744dcd548b3f067811ab89b412ff2fb8b40
Elliott Gorrell <elliottgorrell1@gmail.com>
2018-10-05 17:08:25 +1000

    sorting now implemented

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,8 @@ class App extends Component {
       <div className="App">
         <header className="AppHeader">
           <img src={logo} className="AppLogo" alt="logo" />
-          <p style={{ gridRow: '2' }}><b>5</b> Hotels in <b>Sydney</b></p>
-          <div style={{ gridRow: '2'}}>
-            <select style={{ float: 'right', width: '100px' }}>
-              <option value="low">low-high</option>
-              <option value="high">high-low</option>
-            </select>
-          </div>
         </header>
-        <AccomList />
+        <AccomList ref="resultsList" />
       </div>
     );
   }
--- src/components/AccomList.jsx
+++ src/components/AccomList.jsx
@@ -7,7 +7,24 @@ import dummyData from '../assets/dummyData';
 class AccomList extends Component {
   constructor(props) {
     super(props);
-    this.state = { results: dummyData };
+    this.state = { 
+      results: dummyData,
+      sortingOrder: 'default'
+    };
+  }
+
+  sortByHigh = (list) => {
+    return list.sort((a, b) => a.price - b.price);
+  }
+  sortByLow = (list) => {
+    return list.sort((a, b) => b.price - a.price);
+  } 
+
+  onSort = (event) => {
+    const currentList = this.state.results;
+    this.setState({sortingOrder: event.target.value});
+    if(this.state.sortingOrder === 'low') this.setState({results: this.sortByLow(currentList)});
+    else this.setState({results: this.sortByHigh(currentList)});
   }
 
   render() {
@@ -15,6 +32,16 @@ class AccomList extends Component {
 
     return (
       <div>
+        <div className="topPane">
+          <p><b>5</b> Hotels in <b>Sydney</b></p>
+          <div>
+            <select className="sortSelector" style={{ float: 'right', width: '100px' }} onChange={this.onSort} value={this.state.sortingOrder}>
+              <option disabled selected value value="default"> -- Sort By Price -- </option>
+              <option value="low">low-high</option>
+              <option value="high">high-low</option>
+            </select>
+          </div>
+        </div>
         {hotels.map(hotel => <Result key={hotel.id} {...hotel} className="result" />)}
       </div>
     );
